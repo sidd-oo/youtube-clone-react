fd501807b18bf2eb554419cbf56b749eb86923ba
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2023-04-28 11:06:42 +0530

    feat: suggestion of videos in the watch page

--- src/components/WatchPage/WatchPage.js
+++ src/components/WatchPage/WatchPage.js
@@ -6,6 +6,9 @@ import LiveChat from '../LiveChat';
 import VideoPlayer from './VideoPlayer';
 import CommentsThread from '../CommentsThreads/CommentsThread';
 import VideoInfo from './VideoInfo';
+import axios from 'axios';
+import { VIDEO_LIST_URL } from '../../utils/constants';
+import SuggestionVideoCard from "./SuggestionVideoCard";
 
 const WatchPage = () => {
 
@@ -13,10 +16,20 @@ const WatchPage = () => {
   const videoID = params.get("v");
   const dispatch = useDispatch();
   const [liveBroadcastContent, setLiveBroadcastContent] = useState(false);
+  const [videoList, setVideoList] = useState([]);
+
 
   useEffect(() => {
     dispatch(closeSidebar());
   }, [dispatch]);
+
+  useEffect(() => {
+    const fetchVideoList = async () => {
+      const res = await axios.get(VIDEO_LIST_URL);
+      setVideoList(res.data.items);
+    }
+    fetchVideoList();
+  })
 
   return (
     <div className='flex flex-col w-[85%] mx-auto items-center'>
@@ -24,9 +37,14 @@ const WatchPage = () => {
         <div className='flex flex-col w-[70%] h-[500px]'>
           <VideoPlayer videoID={videoID} />
         </div>
-       {liveBroadcastContent && <div className="w-[30%] ml-5 border-2 border-gray-900">
-          <LiveChat />
-        </div>}
+        {liveBroadcastContent ?
+          <div className="w-[30%] ml-5 border-2 border-gray-900">
+            <LiveChat />
+          </div> : (<div className="w-[30%] ml-5">
+            {videoList.map((video) => {
+              return <SuggestionVideoCard key={video.id} video={video} />
+            })}
+          </div>)}
       </div>
       <div className='flex flex-row m-5 h-[70vh] w-full justify-between'>
         <div className='flex flex-col w-[70%]'>
@@ -35,15 +53,16 @@ const WatchPage = () => {
           />
         </div>
         <div className="w-[30%]">
-
+        {(liveBroadcastContent === true) && <div className="ml-5">
+            {videoList.map((video) => {
+              return <SuggestionVideoCard key={video.id} video={video} />
+            })}
+          </div>}
         </div>
       </div>
-      <div className='flex flex-row m-5 mt-8 h-[70vh] w-full justify-between'>
+      <div className='flex flex-row h-[70vh] w-full justify-between'>
         <div className='w-[70%]'>
           <CommentsThread videoID={videoID} />
-        </div>
-        <div className="w-[30%] ml-5 border-2 border-gray-900">
-          Suggestion of Videos goes here
         </div>
       </div>
       <div></div>
